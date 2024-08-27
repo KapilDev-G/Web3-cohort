f00a3ba6b29cf80971e55f2a1fcf88416e2b0386
Kapil <112493616+KapilDev-G@users.noreply.github.com>
2024-08-27 14:06:44 +0530

    Delete zerodha directory

--- zerodha/script.js
+++
@@ -1,7 +0,0 @@
-function returns(numberOfYears, base, yearlyGains) {
-    return (base* numberOfYears* yearlyGains) / 100
-}
-
-let base = document.getElementById("capi");
-let numberOfYears = document.getElementById("years");
-let yearlyGains = document.getElementById("rate");
